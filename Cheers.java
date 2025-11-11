30e0fdeba61252e095da23e73f233427b7e97b6d
Gili Gerson <gili.gerson@post.runi.ac.il>
2025-11-11 23:55:13 +0200

    test5

--- Cheers.java
+++ Cheers.java
@@ -1,32 +1,23 @@
-//feedback
-
-/*
-Great work boaz!
-*/
-
-
 // Prints a crowd cheering output.
 public class Cheers {
-        public static void main(String[] args) {
-	    // Replace this comment with your code
-            String name = args[0];
-            int num = Integer.parseInt(args[1]);
-            String anLetters = "AEHFHILMNORSK";
+    public static void main(String[] args) {
+        String name = args[0].toUpperCase();
+        int num = Integer.parseInt(args[1]);
+        String anLetters = "AEFHILMNORSX";
 
-            for (int i = 0 ; i < name.length() ; i++) {
-                 char letter = name.charAt(i);
-                if (anLetters.indexOf(letter) >= 0) {
-                System.out.println("Give me an: " + letter + ":" + letter + "!");
-                } else {
-                System.out.println("Give me a: " + letter + ":" + letter + "!");
+        for (int i = 0; i < name.length(); i++) {
+            char letter = name.charAt(i);
+            if (anLetters.indexOf(letter) >= 0) {
+                System.out.println("Give me an " + letter + ": " + letter + "!");
+            } else {
+                System.out.println("Give me a " + letter + ": " + letter + "!");
             }
-
-            }
-               
-            String spell = name + "!!!";
-            System.out.println("What does that spell?"); 
-            for (int i = 0; i < num; i++) {
-                System.out.println(spell);
-            } 
         }
+
+        System.out.println("What does that spell?");
+        String spell = name + "!!!";
+        for (int i = 0; i < num; i++) {
+            System.out.println(spell);
+        }
+    }
 }

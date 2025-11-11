75a9153c45d9d5eeb41436bcfa789f2f54b8bf24
Gili Gerson <gili.gerson@post.runi.ac.il>
2025-11-12 01:12:38 +0200

    test8

--- CalcPi.java
+++ CalcPi.java
@@ -1,30 +1,24 @@
 public class CalcPi {
-    public static void main(String [] args) { 
-        int num = Integer.parseInt(args[0]);
-        int denominator1 = 1;
-        int denominator2 = 3;
-        int numerator = 1;
-        double totalSum = 0.0;
+    public static void main(String [] args) {
+        
+        int numTerms = Integer.parseInt(args[0]);
+        double sum = 0.0;
 
-        for (int i = 1; i < num; i += 2) {
-            totalSum += (double) numerator / denominator1;
-            denominator1 += 4;
+        for (int i = 0; i < numTerms; i++) {
+            
+            double denominator = 2 * i + 1;
+            double term = 1.0 / denominator;
+
+            if (i % 2 == 0) {
+                sum += term;
+            } else {
+                sum -= term;
+            }
         }
 
-        for (int i = 0; i < num; i += 2) {
-            totalSum -= (double) numerator / denominator2;
-            denominator2 += 4;
-        }
-
-        double finalSum = ( totalSum) * 4;
+        double finalSum = sum * 4;
 
         System.out.println("pi according to Java: " + Math.PI);
         System.out.println("pi, approximated: " + finalSum);
     }
 }
-
-
-
-
-
-
